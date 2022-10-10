08ab8093e57077f457435497d7b699ca97c5880f
helensejas <sejas.jhesica.867@gmail.com>
2022-10-10 00:18:47 -0400

    cambios en vista entrenador

--- application/views/listaEntrenador.php
+++ application/views/listaEntrenador.php
@@ -127,23 +127,6 @@
                     </li>
 
                     <li class="menu single-menu">
-                        <a href="#tables" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg>
-                                <span>Estudiantes</span>
-                            </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="tables"  data-parent="#topAccordion">
-                            <li>
-                          <?php echo form_open_multipart('estudiante/index'); ?>
-                        <button type="submit" class="btn btn-outline-warning">Ver Estudiantes</button>
-                        <?php echo form_close(); ?>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="menu single-menu">
                         <a href="#page" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
@@ -212,16 +195,9 @@
                         <a href="#more" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-                                <span>Inscribir</span>
+                                <span>reportes</span>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="more" data-parent="#topAccordion">
-                            <li>
-                                <a href="dragndrop_dragula.html"> Inscribir Nuevo Alumno</a>
-                            </li>
 
-                         
                         </ul>
                     </li>
                 </ul>
@@ -253,7 +229,7 @@
 $indice=1;
 foreach ($entrenador->result() as $row) {
 ?>
-	  <div class="card component-card_4 bg-info" style="max-width: 13rem;">
+	  <div class="card component-card_4 bg-info" style="max-width: 15rem;">
    <img class="card-img-top" src="<?php echo base_url(); ?>assets/img/usuario.png" > 
   <div class="card-header">Entrenador</div>
   <div class="card-body">
